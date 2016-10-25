32b3c054ad4d59d0a6a72a5dc7199a0f8c6c5e1e
spekek <spekek@uni.coventry.ac.uk>
2016-10-25 13:11:07 +0100

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -23,7 +23,7 @@ class app(tk.tk):
 
 #this allows you to select different frames to bring to the top
         self.frames = {}
-        for F in (Menu, PageOne, PageTwo):
+        for F in (StatPage, PageOne, PageTwo):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -37,3 +37,59 @@ class app(tk.tk):
     def show_frame(self, page_name):
         frame = self.frames[page_name]
         frame.tkraise()
+
+#this is the menu
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Tic-Tac-Toe", font=TITLE_FONT)
+        label.pack(side="top", fill="x", pady=10)
+
+        button1 = tk.Button(self, text="Computer", bg="black", fg="white", command=lambda: controller.show_frame("PageOne")).pack()
+        button2 = tk.Button(self, text="Multiplayer", bg="black", fg="white", command=lambda: controller.show_frame("PageTwo")).pack()
+
+#computer page where you can play against computer
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg="black")
+        self.controller = controller
+        label = tk.Label(self, text="Playing Computer", font=TITLE_FONT, bg="black", fg="white").grid(column=1, row=0, columnspan=3) 
+        button = tk.Button(self, text="Menu", bg="black", fg="white", command=lambda: controller.show_frame("StartPage")).grid(column=2, row=5) 
+
+        button1 = tk.Button(self, height="10", width="20").grid(column=1, row=1) 
+        button2 = tk.Button(self, bg="yellow", height="10", width="20").grid(column=2, row=1) 
+        button3 = tk.Button(self, bg="black", height="10", width="20").grid(column=3, row=1) 
+        button4 = tk.Button(self, bg="white", height="10", width="20").grid(column=1, row=2) 
+        button5 = tk.Button(self, bg="orange", height="10", width="20").grid(column=2, row=2) 
+        button6 = tk.Button(self, bg="pink", height="10", width="20").grid(column=3, row=2) 
+        button7 = tk.Button(self, bg="blue", height="10", width="20").grid(column=1, row=3) 
+        button8 = tk.Button(self, bg="red", height="10", width="20").grid(column=2, row=3) 
+
+#multiplayer page
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg="black")
+        self.controller = controller
+        label = tk.Label(self, text="Playing Multiplayer", font=TITLE_FONT, bg="black", fg="white").grid(column=1, row=0, columnspan=3) 
+        button = tk.Button(self, text="Menu", bg="black", fg="white", command=lambda: controller.show_frame("StartPage")).grid(column=2, row=5) 
+        
+        button1 = tk.Button(self, height="10", width="20").grid(column=1, row=1) 
+        button2 = tk.Button(self, bg="yellow", height="10", width="20").grid(column=2, row=1) 
+        button3 = tk.Button(self, bg="black", height="10", width="20").grid(column=3, row=1) 
+        button4 = tk.Button(self, bg="white", height="10", width="20").grid(column=1, row=2) 
+        button5 = tk.Button(self, bg="orange", height="10", width="20").grid(column=2, row=2) 
+        button6 = tk.Button(self, bg="pink", height="10", width="20").grid(column=3, row=2) 
+        button7 = tk.Button(self, bg="blue", height="10", width="20").grid(column=1, row=3) 
+        button8 = tk.Button(self, bg="red", height="10", width="20").grid(column=2, row=3) 
+        button9 = tk.Button(self, bg="purple", height="10", width="20").grid(column=3, row=3)
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
+
+
+     
