8c427a5003594d6e7dc627a0a1cc267ecd75e198
hudsonh1 <hudsonh@coventry.ac.uk>
2016-11-25 09:45:36 +0000

    Add files via upload
    basic board for text based Tic Tac Toe game

---
+++ pythontextBased.py
@@ -0,0 +1,12 @@
+#tic tac toe game
+
+board = [0,1,2,
+         3,4,5,
+         6,7,8]
+
+print (board[0],'|',board[1],'|',board[2])
+print ('----------')
+print (board[3],'|',board[4],'|',board[5])
+print ('----------')
+print (board[6],'|',board[7],'|',board[8])
+
