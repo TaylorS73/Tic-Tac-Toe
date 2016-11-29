b50489ee4f29dce166f6defecb3293dbe54de8fc
spekek <spekek@uni.coventry.ac.uk>
2016-11-29 13:33:27 +0000

    Update Text-Based Tic-Tac-Toe.py

--- Text-Based Tic-Tac-Toe.py
+++ Text-Based Tic-Tac-Toe.py
@@ -1,6 +1,6 @@
 import time, sys
 from random import randint
-#this will allow the user to enter in X and O into this grid spaces
+#this will allow the user to enter in X and O into the grid spaces
 pos1 = ' '
 pos2 = ' '
 pos3 = ' '
@@ -11,55 +11,55 @@ pos7 = ' '
 pos8 = ' '
 pos9 = ' '
 
-gridDict = {1:pos1, 2:pos2, 3:pos3, 4:pos4, 5:pos5, 6:pos6, 7:pos7, 8:pos8, 9:pos9}
-#if player has three Xs in a row haveWonX will become true triggering the if statement and the bottom on the code 
+grid = {1:pos1, 2:pos2, 3:pos3, 4:pos4, 5:pos5, 6:pos6, 7:pos7, 8:pos8, 9:pos9}
+#if player has three X's in a row haveWonX will become true triggering the if statement at the bottom of the code 
 haveWonO = False
 haveWonX = False
 
 counter = 0 # the counter counts how many moves there have been. If there has been 9 moves it has to be a tie.
 def gameAI():
 
-'''this will lay out the grid for the tic tac toe game'''   
-    if ((gridDict[2] == 'X' and gridDict[3] == 'X') or (gridDict[4] == 'X' and gridDict[7] == 'X') or (gridDict[5] == 'X' and gridDict[9] == 'X')) and gridDict[1] != 'O':
+'''this checks where Ai moves'''   
+    if ((grid[2] == 'X' and grid[3] == 'X') or (grid[4] == 'X' and grid[7] == 'X') or (grid[5] == 'X' and grid[9] == 'X')) and grid[1] != 'O':
         return 1
-    elif ((gridDict[1] == 'X' and gridDict[3] == 'X') or (gridDict[5] == 'X' and gridDict[8] == 'X')) and gridDict[2] != 'O':
+    elif ((grid[1] == 'X' and grid[3] == 'X') or (grid[5] == 'X' and grid[8] == 'X')) and grid[2] != 'O':
         return 2
-    elif ((gridDict[1] == 'X' and gridDict[2] == 'X') or (gridDict[6] == 'X' and gridDict[9] == 'X') or (gridDict[5] == 'X' and gridDict[7] == 'X')) and gridDict[3] != 'O':
+    elif ((grid[1] == 'X' and grid[2] == 'X') or (grid[6] == 'X' and grid[9] == 'X') or (grid[5] == 'X' and grid[7] == 'X')) and grid[3] != 'O':
         return 3
-    elif ((gridDict[1] == 'X' and gridDict[7] == 'X') or (gridDict[5] == 'X' and gridDict[6] == 'X')) and gridDict[4] != 'O':
+    elif ((grid[1] == 'X' and grid[7] == 'X') or (grid[5] == 'X' and grid[6] == 'X')) and grid[4] != 'O':
         return 4
-    elif ((gridDict[1] == 'X' and gridDict[9] == 'X') or (gridDict[2] == 'X' and gridDict[8] == 'X') or (gridDict[3] == 'X' and gridDict[7] == 'X') or (gridDict[4] == 'X' and gridDict[6] == 'X')) and gridDict[5] != 'O':
+    elif ((grid[1] == 'X' and grid[9] == 'X') or (grid[2] == 'X' and grid[8] == 'X') or (grid[3] == 'X' and grid[7] == 'X') or (grid[4] == 'X' and grid[6] == 'X')) and grid[5] != 'O':
         return 5
-    elif ((gridDict[3] == 'X' and gridDict[9] == 'X') or (gridDict[4] == 'X' and gridDict[5] == 'X')) and gridDict[6] != 'O':
+    elif ((grid[3] == 'X' and grid[9] == 'X') or (grid[4] == 'X' and grid[5] == 'X')) and grid[6] != 'O':
         return 6
-    elif ((gridDict[1] == 'X' and gridDict[4] == 'X') or (gridDict[3] == 'X' and gridDict[5] == 'X') or (gridDict[8] == 'X' and gridDict[9] == 'X')) and gridDict[7] != 'O':
+    elif ((grid[1] == 'X' and grid[4] == 'X') or (grid[3] == 'X' and grid[5] == 'X') or (grid[8] == 'X' and grid[9] == 'X')) and grid[7] != 'O':
         return 7
-    elif ((gridDict[2] == 'X' and gridDict[5] == 'X') or (gridDict[7] == 'X' and gridDict[9] == 'X')) and gridDict[8] != 'O':
+    elif ((grid[2] == 'X' and grid[5] == 'X') or (grid[7] == 'X' and grid[9] == 'X')) and grid[8] != 'O':
         return 8
-    elif ((gridDict[1] == 'X' and gridDict[5] == 'X') or (gridDict[3] == 'X' and gridDict[6] == 'X') or (gridDict[7] == 'X' and gridDict[8] == 'X')) and gridDict[9] != 'O':
+    elif ((grid[1] == 'X' and grid[5] == 'X') or (grid[3] == 'X' and grid[6] == 'X') or (grid[7] == 'X' and grid[8] == 'X')) and grid[9] != 'O':
         return 9
         
     while True:
         randInt = randint(1, 9)
 
-        if gridDict[randInt] == 'X' or gridDict[randInt] == 'O':
+        if grid[randInt] == 'X' or grid[randInt] == 'O':
             continue
         else:
-            gridDict[randInt] = 'O'
+            grid[randInt] = 'O'
             break
 def drawGrid():
 
         print('\t')
         print('       ║       ║       ')
-        print('   %s  ║   %s  ║   %s  ' % (' ' + gridDict[1], gridDict[2] + ' ', gridDict[3] + ' '))
+        print('   %s  ║   %s  ║   %s  ' % (' ' + grid[1], grid[2] + ' ', grid[3] + ' '))
         print('       ║       ║       ')
         print(' ══════╬═══════╬═══════')
         print('       ║       ║       ')
-        print('   %s  ║   %s  ║   %s  ' % (' ' + gridDict[4], gridDict[5] + ' ', gridDict[6] + ' '))
+        print('   %s  ║   %s  ║   %s  ' % (' ' + grid[4], grid[5] + ' ', grid[6] + ' '))
         print('       ║       ║       ')
         print(' ══════╬═══════╬═══════')
         print('       ║       ║       ')
-        print('   %s  ║   %s  ║   %s  ' % (' ' + gridDict[7], gridDict[8] + ' ', gridDict[9] + ' '))
+        print('   %s  ║   %s  ║   %s  ' % (' ' + grid[7], grid[8] + ' ', grid[9] + ' '))
         print('       ║       ║       ' + '\n')
 
 print ('\n' + 'This is how the Tic-Tac-Toe grid looks like (starting from position 1 [top-left] all the way to position 9 [bottom-right]):')
@@ -72,17 +72,17 @@ while haveWonO == False or haveWonX == False:
         break
     else:
         player = int(input("Player 1's turn. Please input where you want to place the 'X': "))
-        gridDict[player] = 'X'
+        grid[player] = 'X'
         drawGrid()
-        if ((gridDict[1] == 'X' and gridDict[2] == 'X' and gridDict[3] == 'X')
-        or (gridDict[1] == 'X' and gridDict[4] == 'X' and gridDict[7] == 'X')
-        or (gridDict[1] == 'X' and gridDict[5] == 'X' and gridDict[9] == 'X')
-        or (gridDict[2] == 'X' and gridDict[5] == 'X' and gridDict[8] == 'X')
-        or (gridDict[3] == 'X' and gridDict[6] == 'X' and gridDict[9] == 'X')
-        or (gridDict[3] == 'X' and gridDict[5] == 'X' and gridDict[7] == 'X')
-        or (gridDict[4] == 'X' and gridDict[5] == 'X' and gridDict[6] == 'X')
-        or (gridDict[7] == 'X' and gridDict[8] == 'X' and gridDict[9] == 'X')):
-            haveWonO = True
+        if ((grid[1] == 'X' and grid[2] == 'X' and grid[3] == 'X')
+        or (grid[1] == 'X' and grid[4] == 'X' and grid[7] == 'X')
+        or (grid[1] == 'X' and grid[5] == 'X' and grid[9] == 'X')
+        or (grid[2] == 'X' and grid[5] == 'X' and grid[8] == 'X')
+        or (grid[3] == 'X' and grid[6] == 'X' and grid[9] == 'X')
+        or (grid[3] == 'X' and grid[5] == 'X' and grid[7] == 'X')
+        or (grid[4] == 'X' and grid[5] == 'X' and grid[6] == 'X')
+        or (grid[7] == 'X' and grid[8] == 'X' and grid[9] == 'X')):
+            haveWonO = Truegrid
             break
         counter += 1
 
@@ -92,39 +92,39 @@ while haveWonO == False or haveWonX == False:
     else:
         print ("This is the computer's turn.")
         print ('\n')
-        time.sleep(0.5)
+        time.sleep(0.3)
         sys.stdout.write('Thinking')
-        time.sleep(0.2)
+        time.sleep(0.1)
         sys.stdout.write('.')
-        time.sleep(0.2)
+        time.sleep(0.1)
         sys.stdout.write('.')
-        time.sleep(0.2)
+        time.sleep(0.1)
         sys.stdout.write('.')
-        time.sleep(0.2)
+        time.sleep(0.1)
         sys.stdout.write('.')
-        time.sleep(0.2)
+        time.sleep(0.1)
         sys.stdout.write('.')
 
         print ('\n')
         
         computer = gameAI()
-        gridDict[computer] = 'O'
+        grid[computer] = 'O'
         drawGrid()
-        if ((gridDict[1] == 'O' and gridDict[2] == 'O' and gridDict[3] == 'O')
-        or (gridDict[1] == 'O' and gridDict[4] == 'O' and gridDict[7] == 'O')
-        or (gridDict[1] == 'O' and gridDict[5] == 'O' and gridDict[9] == 'O')
-        or (gridDict[2] == 'O' and gridDict[5] == 'O' and gridDict[8] == 'O')
-        or (gridDict[3] == 'O' and gridDict[6] == 'O' and gridDict[9] == 'O')
-        or (gridDict[3] == 'O' and gridDict[5] == 'O' and gridDict[7] == 'O')
-        or (gridDict[4] == 'O' and gridDict[5] == 'O' and gridDict[6] == 'O')
-        or (gridDict[7] == 'O' and gridDict[8] == 'O' and gridDict[9] == 'O')):
+        if ((grid[1] == 'O' and grid[2] == 'O' and grid[3] == 'O')
+        or (grid[1] == 'O' and grid[4] == 'O' and grid[7] == 'O')
+        or (grid[1] == 'O' and grid[5] == 'O' and grid[9] == 'O')
+        or (grid[2] == 'O' and grid[5] == 'O' and grid[8] == 'O')
+        or (grid[3] == 'O' and grid[6] == 'O' and grid[9] == 'O')
+        or (grid[3] == 'O' and grid[5] == 'O' and grid[7] == 'O')
+        or (grid[4] == 'O' and grid[5] == 'O' and grid[6] == 'O')
+        or (grid[7] == 'O' and grid[8] == 'O' and grid[9] == 'O')):
             haveWonX = True
             break
         counter += 1
 
 if haveWonO == True:
-    print ('Congratulations Player 1 won!')
+    print ('O has won!')
 elif haveWonX == True:
-    print ('Congratulations Player 2 won!')
+    print ('X has won!')
 elif counter == 9:
-    print('It is a tie!')
+    print('It's a tie!')
